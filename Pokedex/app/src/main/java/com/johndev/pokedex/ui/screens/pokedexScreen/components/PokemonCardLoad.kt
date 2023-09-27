3b8c4b904d876fdf6062ad5d290215aa2160ff9f
John Morales <johnta2610@hotmail.com>
2023-09-27 12:05:15 -0600

    ⭐Enhancement: UI/UX Improvements: Borders & Structure Completed

--- Pokedex/app/src/main/java/com/johndev/pokedex/ui/screens/pokedexScreen/components/PokemonCardLoad.kt
+++ Pokedex/app/src/main/java/com/johndev/pokedex/ui/screens/pokedexScreen/components/PokemonCardLoad.kt
@@ -16,28 +16,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.AssistChip
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush.Companion.linearGradient
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import com.johndev.pokedex.common.utils.capitalizeFirstLetter
-import com.johndev.pokedex.common.utils.formatId
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PokemonCardLoad(color: Color) {
     val gradient = listOf(
@@ -45,8 +37,9 @@ fun PokemonCardLoad(color: Color) {
         color.copy(alpha = 0.4f),
         color.copy(alpha = 0.9f),
     )
+
     val transition = rememberInfiniteTransition(label = "")
-    val translateAnimation = transition.animateFloat(
+    val translateAnimation by transition.animateFloat(
         initialValue = 8f,
         targetValue = 1808f,
         animationSpec = infiniteRepeatable(
@@ -56,55 +49,51 @@ fun PokemonCardLoad(color: Color) {
             )
         ), label = ""
     )
-    val brush = linearGradient(
-        colors = gradient,
-        start = Offset(200f, 200f),
-        end = Offset(x = translateAnimation.value, y = translateAnimation.value)
-    )
+
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .height(100.dp)
-            .border(width = 2.dp, color = Color(0xffF5F5F5))
+            .border(width = 2.dp, color = Color(0xffF5F5F5), shape = RoundedCornerShape(8.dp))
     ) {
-        Row(
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Column(
-                modifier = Modifier
-                    .weight(3f)
-                    .fillMaxSize()
-                    .padding(16.dp),
-                verticalArrangement = Arrangement.Center
-            ) {
+        Row(modifier = Modifier.fillMaxSize()) {
+            val gradientBrush = linearGradient(
+                colors = gradient,
+                start = Offset(200f, 200f),
+                end = Offset(translateAnimation, translateAnimation)
+            )
+
+            val cardModifier = Modifier
+                .weight(3f)
+                .fillMaxSize()
+                .padding(16.dp)
+
+            val spacerModifier = Modifier
+                .background(brush = gradientBrush, shape = RoundedCornerShape(8.dp))
+                .weight(1f)
+
+            val cardTextStyle = MaterialTheme.typography.titleMedium.copy(color = Color.Transparent)
+
+            Column(modifier = cardModifier, verticalArrangement = Arrangement.Center) {
                 Row(
                     modifier = Modifier.weight(1f),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(text = "      ", modifier = spacerModifier, style = cardTextStyle)
+                    Spacer(modifier = Modifier.width(24.dp))
+                    Text(text = "      ", modifier = spacerModifier, style = cardTextStyle)
+                }
+                Row(
+                    modifier = Modifier.weight(1f),
+                    horizontalArrangement = Arrangement.spacedBy(24.dp),
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Text(
-                        text = "",
-                        modifier = Modifier.background(brush).weight(1f),
-                        style = MaterialTheme.typography.bodyLarge
-                    )
-                    Spacer(modifier = Modifier.width(24.dp))
-                    Text(
-                        text = "",
-                        modifier = Modifier.background(brush).weight(1f),
-                        style = MaterialTheme.typography.titleMedium
-                    )
-                }
-                Row(
-                    modifier = Modifier.weight(1f),
-                    horizontalArrangement = Arrangement.spacedBy(8.dp)
-                ) {
-                    Box(
-                        modifier = Modifier.background(brush).weight(1f),
-                    )
-                    Box(
-                        modifier = Modifier.background(brush).weight(1f),
-                    )
+                    Text(text = "      ", modifier = spacerModifier, style = cardTextStyle)
+                    Text(text = "      ", modifier = spacerModifier, style = cardTextStyle)
                 }
             }
+
             Column(
                 modifier = Modifier.weight(1f),
                 verticalArrangement = Arrangement.Center,
@@ -124,8 +113,10 @@ fun PokemonCardLoad(color: Color) {
                         )
                 ) {
                     Box(
-                        modifier = Modifier.width(70.dp).height(70.dp).background(brush).align(
-                            Alignment.Center),
+                        modifier = Modifier
+                            .size(70.dp)
+                            .background(brush = gradientBrush, shape = RoundedCornerShape(8.dp))
+                            .align(Alignment.Center)
                     )
                 }
             }
